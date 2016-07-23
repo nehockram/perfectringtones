805e2af2819afa79a5eab5c5ac78fc787731536e
cococohen <cococohen@gmail.com>
2016-07-23 02:27:26 -0700

    ok

---
+++ pr.js
@@ -0,0 +1,6 @@
+if (localStorage.pagecount) {
+    localStorage.pagecount = Number(localStorage.pagecount) + 1;
+} else {
+    localStorage.pagecount = 1;
+}
+document.write("Visits: " + localStorage.pagecount + " time(s).");
